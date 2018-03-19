aaf970c4276c368df70d9b531fc2a455efb05d68
nnggdd <doya1103@gmail.com>
2018-03-19 13:17:32 +0700

    03-19 save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
+  end
+
+  def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super # Use the default one
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
+#require_relative 'boot'
 
 require 'rails/all'
 
@@ -9,10 +10,9 @@ Bundler.require(*Rails.groups)
 module Photolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
